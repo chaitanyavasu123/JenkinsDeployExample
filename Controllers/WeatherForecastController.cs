fe6d5a1e271cae1c4ec6564a7424e8f03a69723c
Chaitanya Vasu <chaitanya.vasu@nagarro.com>
2024-04-01 14:24:16 +0530

    Adding another value

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace JenkinsDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Vasu"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Vasu", "Mayank"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
